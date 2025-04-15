d333fb933b2b163071d413a3b76ba9fd14cd58fe
Kiky-88 <ss598343479@gmail.com>
2025-04-15 17:05:44 +0800

    测试提交，处理 VS Code 卡住

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -120,4 +120,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# just testing push
+# just testing push###
+print("hello")
+#djshfjkshdjk
+#sdfdsdfsdfsdsffsdfsdfsdfs
